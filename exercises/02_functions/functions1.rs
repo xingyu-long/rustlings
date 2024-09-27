afdff3e8a06d87faf2036e92914e14eb640fe3bc
Xingyu Long <xingyulong97@gmail.com>
2024-09-27 15:59:47 -0700

    08_enums

---
+++ exercises/02_functions/functions1.rs
@@ -0,0 +1,10 @@
+// TODO: Add some function with the name `call_me` without arguments or a return value.
+
+fn main() {
+    call_me(); // Don't change this line
+}
+
+
+fn call_me() {
+    println!("here is function: call_me")
+}
